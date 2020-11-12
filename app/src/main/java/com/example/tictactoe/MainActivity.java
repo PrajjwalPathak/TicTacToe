5042216d5e141e66c728abbe34c6e1007f4ca2cc
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-11-12 09:38:53 +0530

    Add Android App

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+        Log.i("Tag", "Button Pressed");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
